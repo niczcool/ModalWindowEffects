c0ed02a603feaf338c19db3068311e7eab18aba8
Fransnico <fransnico@gmail.com>
2014-05-07 22:43:06 +0700

    Update modalEffects.js

--- js/modalEffects.js
+++ js/modalEffects.js
@@ -12,11 +12,11 @@ var ModalEffects = (function() {
 
 	function init() {
 		
-		var body = document.querySelector( 'body' );
 		var overlay = document.querySelector( '.md-overlay' );
 
 		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
 
+			var body = document.querySelector( 'body' );
 			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
 				close = modal.querySelector( '.md-close' );
 
